18512287be726d5342aa4aafde7f049a42bcc1a2
VoxelError <61365087+Asterverse@users.noreply.github.com>
2024-03-25 11:33:11 -0700

    Separate remaining code in setInterval into functions

--- src/index.js
+++ src/index.js
@@ -12,35 +12,33 @@ const to_radians = (num) => (num * pi) / 180
 const distance = (x1, y1, x2, y2) => Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
 const to_array = (length, fill = 0) => Array(length).fill(fill)
 
-const fps = 60
-const ship_size = 30
+const high_score = () => localStorage.getItem("highscore") ?? 0
+const set_high_score = (value) => localStorage.setItem("highscore", value)
 
 const debug_collisions = false
 const debug_dot = false
 
-const high_score = () => localStorage.getItem("highscore") ?? 0
-const set_high_score = (value) => localStorage.setItem("highscore", value)
+const asteroids_list = []
 
 let ship
-let text
+let text_content
 let text_alpha = 0
 let game_score
 let game_level
 let game_lives
 let flicker = 0
 
-const asteroids_list = []
-
 const default_ship = () => {
 	return {
 		x: canvas.width / 2,
 		y: canvas.height / 2,
-		r: ship_size / 2,
+		r: 15,
 		a: to_radians(90),
 		blink_num: 15,
 		blink_time: 6,
 		can_shoot: true,
 		is_dead: false,
+		has_crashed: false,
 		explode_time: 0,
 		lasers: [],
 		rot: 0,
@@ -55,8 +53,9 @@ const default_ship = () => {
 document.addEventListener("keydown", (event) => {
 	if (ship.is_dead) return
 	event.code == "ArrowUp" && (ship.is_thrusting = true)
-	event.code == "ArrowRight" && (ship.rot = -to_radians(180) / fps)
-	event.code == "ArrowLeft" && (ship.rot = to_radians(180) / fps)
+	event.code == "ArrowRight" && (ship.rot = -to_radians(3))
+	event.code == "ArrowLeft" && (ship.rot = to_radians(3))
+	if (ship.explode_time > 0) return
 	event.code == "Space" && shoot_laser()
 })
 
@@ -68,10 +67,16 @@ document.addEventListener("keyup", (event) => {
 	event.code == "Space" && (ship.can_shoot = true)
 })
 
+const new_life = () => {
+	game_lives--
+	ship = default_ship()
+	game_lives <= 0 && game_over()
+}
+
 const new_level = () => {
 	asteroids_list.length = 0
 	game_level++
-	text = `Level ${game_level}`
+	text_content = `Level ${game_level}`
 	text_alpha = 1.0
 	to_array(2 + game_level).forEach(() => {
 		let x, y
@@ -93,7 +98,7 @@ const new_game = () => {
 
 const game_over = () => {
 	ship.is_dead = true
-	text = "Game Over"
+	text_content = "Game Over"
 	text_alpha = 1.0
 }
 
@@ -129,34 +134,16 @@ const destroy_asteroid = (asteroid, index) => {
 	fxHit.play()
 }
 
-const draw_ship = (x, y, a) => {
-	const { r } = ship
-	context.beginPath()
-	context.moveTo(
-		x + (4 / 3) * r * cos(a),
-		y - (4 / 3) * r * sin(a)
-	)
-	context.lineTo(
-		x - ((cos(a) * 2 / 3) + sin(a)) * r,
-		y + ((sin(a) * 2 / 3) - cos(a)) * r
-	)
-	context.lineTo(
-		x - ((cos(a) * 2 / 3) - sin(a)) * r,
-		y + ((sin(a) * 2 / 3) + cos(a)) * r
-	)
-	context.closePath()
-	context.strokeStyle = "white"
-	context.stroke()
-}
-
 const shoot_laser = () => {
 	const { x, y, r, a } = ship
+	const laser_speed = 8
+
 	if (ship.can_shoot && ship.lasers.length < 10) {
 		ship.lasers.push({
 			x: x + (4 / 3) * r * cos(a),
 			y: y - (4 / 3) * r * sin(a),
-			xv: 15 * cos(a),
-			yv: -15 * sin(a),
+			xv: laser_speed * cos(a),
+			yv: -laser_speed * sin(a),
 			dist: 0,
 			explode_time: 0
 		})
@@ -220,38 +207,146 @@ const draw_asteroids = () => asteroids_list.forEach((asteroid) => {
 const draw_lasers = () => ship.lasers.forEach((laser) => {
 	if (laser.explode_time != 0) return
 	context.beginPath()
-	context.rect(laser.x, laser.y, 3, 3)
+	context.arc(laser.x, laser.y, 1.8, 0, 2 * pi)
+	context.fillStyle = "white"
 	context.fill()
 })
 
-const screen_wrap_ship = () => {
+const draw_text = () => {
+	context.textAlign = "center"
+	context.textBaseline = "middle"
+	context.fillStyle = `rgba(255, 255, 255, ${text_alpha})`
+	context.font = `small-caps 40px Emulogic`
+	if (text_alpha > 0) {
+		context.fillText(text_content, canvas.width / 2, canvas.height * 0.75)
+		text_alpha -= 0.008
+	} else {
+		context.beginPath()
+	}
+}
+
+const thrust_ship = () => {
+	const { x, y, r, a } = ship
+
+	ship.thrust.x += (cos(a) * 0.083)
+	ship.thrust.y -= (sin(a) * 0.083)
+	fxThrust.play()
+
+	flicker++
+	flicker > 4 && (flicker = 0)
+
+	if (ship.explode_time <= 0 && ship.blink_num % 2 == 0) {
+		context.beginPath()
+		context.moveTo(
+			x - r * (2 / 3 * cos(a) + 0.5 * sin(a)),
+			y + r * (2 / 3 * sin(a) - 0.5 * cos(a))
+		)
+		context.lineTo(
+			x - r * 5 / 3 * cos(a),
+			y + r * 5 / 3 * sin(a)
+		)
+		context.lineTo(
+			x - r * (2 / 3 * cos(a) - 0.5 * sin(a)),
+			y + r * (2 / 3 * sin(a) + 0.5 * cos(a))
+		)
+		context.closePath()
+		context.strokeStyle = "white"
+		flicker > 2 && context.stroke()
+	}
+}
+
+const brake_ship = () => {
+	const friction = 0.006
+	ship.thrust.x -= (ship.thrust.x * friction)
+	ship.thrust.y -= (ship.thrust.y * friction)
+	fxThrust.stop()
+}
+
+const break_asteroids = () => asteroids_list.forEach((asteroid, index) => {
+	ship.lasers.forEach((laser) => {
+		if (laser.explode_time == 0 && distance(asteroid.x, asteroid.y, laser.x, laser.y) < asteroid.r) {
+			destroy_asteroid(asteroid, index)
+			laser.explode_time = 6
+		}
+	})
+})
+
+const ship_crash = () => {
+	asteroids_list.forEach((asteroid, index) => {
+		if (distance(ship.x, ship.y, asteroid.x, asteroid.y) < ship.r + asteroid.r) {
+			destroy_asteroid(asteroid, index)
+			ship.explode_time = 20
+			ship.can_shoot = false
+			fxExplode.play()
+		}
+	})
+}
+
+const move_lasers = () => ship.lasers.forEach((laser, index) => {
+	laser.dist > (canvas.width * 0.6) && ship.lasers.splice(index, 1)
+
+	if (laser.explode_time > 0) {
+		laser.explode_time--
+		laser.explode_time == 0 && ship.lasers.splice(index, 1)
+	} else {
+		laser.x += laser.xv
+		laser.y += laser.yv
+		laser.dist += Math.hypot(laser.xv, laser.yv)
+	}
+
+	laser.x < 0 && (laser.x = canvas.width)
+	laser.x > canvas.width && (laser.x = 0)
+	laser.y < 0 && (laser.y = canvas.height)
+	laser.y > canvas.height && (laser.y = 0)
+})
+
+const move_asteroids = () => asteroids_list.forEach((asteroid) => {
+	asteroid.x += asteroid.xv
+	asteroid.y += asteroid.yv
+
+	asteroid.x < -asteroid.r && (asteroid.x = canvas.width + asteroid.r)
+	asteroid.x > canvas.width + asteroid.r && (asteroid.x = -asteroid.r)
+	asteroid.y < -asteroid.r && (asteroid.y = canvas.height + asteroid.r)
+	asteroid.y > canvas.height + asteroid.r && (asteroid.y = -asteroid.r)
+})
+
+const move_ship = () => {
+	ship.is_thrusting && !ship.is_dead ? thrust_ship() : brake_ship()
+
 	ship.x < 0 - ship.r && (ship.x = canvas.width + ship.r)
 	ship.x > canvas.width + ship.r && (ship.x = 0 - ship.r)
 	ship.y < 0 - ship.r && (ship.y = canvas.height + ship.r)
 	ship.y > canvas.height + ship.r && (ship.y = 0 - ship.r)
 }
 
-setInterval(() => {
-	game_lives ?? new_game()
-	ship.is_dead && text_alpha <= 0 && new_game()
-	!asteroids_list.length && new_level()
+const draw_hull = (x, y, a) => {
+	const { r } = ship
+	context.beginPath()
+	context.moveTo(
+		x + (4 / 3) * r * cos(a),
+		y - (4 / 3) * r * sin(a)
+	)
+	context.lineTo(
+		x - ((cos(a) * 2 / 3) + sin(a)) * r,
+		y + ((sin(a) * 2 / 3) - cos(a)) * r
+	)
+	context.lineTo(
+		x - ((cos(a) * 2 / 3) - sin(a)) * r,
+		y + ((sin(a) * 2 / 3) + cos(a)) * r
+	)
+	context.closePath()
+	context.strokeStyle = "white"
+	context.stroke()
+}
 
-	draw_space()
-	draw_score()
-	draw_high_score()
-	draw_asteroids()
-	draw_lasers()
-
-	to_array(game_lives).forEach((v, i) => draw_ship((i * 36) + 30, 30, pi / 2))
-	debug_collisions && draw_collision(ship.x, ship.y, ship.r)
-	debug_dot && draw_dot()
-
+const draw_ship = () => {
 	if (ship.explode_time > 0) {
 		context.beginPath()
 		context.arc(ship.x, ship.y, ship.r * 1.5, 0, pi * 2, false)
+		context.fillStyle = "white"
 		context.fill()
 	} else {
-		ship.blink_num % 2 == 0 && !ship.is_dead && draw_ship(ship.x, ship.y, ship.a)
+		ship.blink_num % 2 == 0 && !ship.is_dead && draw_hull(ship.x, ship.y, ship.a)
 		ship.blink_num > 0 && ship.blink_time--
 		if (ship.blink_time == 0) {
 			ship.blink_time = 6
@@ -259,108 +354,43 @@ setInterval(() => {
 		}
 	}
 
-	if (ship.is_thrusting && !ship.is_dead) {
-		const { x, y, r, a } = ship
-
-		ship.thrust.x += (cos(a) * 0.083)
-		ship.thrust.y -= (sin(a) * 0.083)
-		fxThrust.play()
-
-		if (ship.explode_time <= 0 && ship.blink_num % 2 == 0) {
-			context.beginPath()
-			context.moveTo(
-				x - r * (2 / 3 * cos(a) + 0.5 * sin(a)),
-				y + r * (2 / 3 * sin(a) - 0.5 * cos(a))
-			)
-			context.lineTo(
-				x - r * 5 / 3 * cos(a),
-				y + r * 5 / 3 * sin(a)
-			)
-			context.lineTo(
-				x - r * (2 / 3 * cos(a) - 0.5 * sin(a)),
-				y + r * (2 / 3 * sin(a) + 0.5 * cos(a))
-			)
-			context.closePath()
-			context.strokeStyle = "white"
-			flicker > 2 && context.stroke()
-		}
-
-		flicker++
-		flicker > 4 && (flicker = 0)
-	} else {
-		ship.thrust.x -= (ship.thrust.x * 0.006)
-		ship.thrust.y -= (ship.thrust.y * 0.006)
-		fxThrust.stop()
-	}
-
-	if (text_alpha > 0) {
-		context.textAlign = "center"
-		context.textBaseline = "middle"
-		context.fillStyle = `rgba(255, 255, 255, ${text_alpha})`
-		context.font = `small-caps 40px Emulogic`
-		context.fillText(text, canvas.width / 2, canvas.height * 0.75)
-		text_alpha -= 0.008
-	}
-
-	asteroids_list.forEach((asteroid, index) => {
-		ship.lasers.forEach((laser) => {
-			if (laser.explode_time == 0 && distance(asteroid.x, asteroid.y, laser.x, laser.y) < asteroid.r) {
-				destroy_asteroid(asteroid, index)
-				laser.explode_time = 6
-			}
-		})
-	})
-
 	if (ship.explode_time > 0) {
 		ship.explode_time--
-		if (ship.explode_time == 0) {
-			game_lives--
-			ship = default_ship()
-			game_lives <= 0 && game_over()
-		}
+		ship.explode_time == 0 && new_life()
 	} else {
 		ship.a += ship.rot
 		ship.x += ship.thrust.x / 2
 		ship.y += ship.thrust.y / 2
 
-		if (!ship.is_dead && ship.blink_num == 0) {
-			asteroids_list.forEach((asteroid, index) => {
-				if (distance(ship.x, ship.y, asteroid.x, asteroid.y) < ship.r + asteroid.r) {
-					destroy_asteroid(asteroid, index)
-					ship.explode_time = 20
-					fxExplode.play()
-				}
-			})
-		}
+		!ship.is_dead && ship.blink_num == 0 && ship_crash()
 	}
 
-	screen_wrap_ship()
+	debug_collisions && draw_collision(ship.x, ship.y, ship.r)
+	debug_dot && draw_dot()
+}
 
-	ship.lasers.forEach((laser, index) => {
-		(laser.dist > canvas.width * 0.6) && ship.lasers.splice(index, 1)
+const draw_lives = () => to_array(game_lives).forEach((v, i) => draw_hull((i * 36) + 30, 30, pi / 2))
 
-		if (laser.explode_time > 0) {
-			laser.explode_time--
-			laser.explode_time == 0 && ship.lasers.splice(index, 1)
-		} else {
-			laser.x += laser.xv
-			laser.y += laser.yv
-			laser.dist += Math.hypot(laser.xv, laser.yv)
-		}
+setInterval(() => {
+	game_lives ?? new_game()
+	ship.is_dead && text_alpha <= 0 && new_game()
+	!asteroids_list.length && new_level()
 
-		laser.x < 0 && (laser.x = canvas.width)
-		laser.x > canvas.width && (laser.x = 0)
-		laser.y < 0 && (laser.y = canvas.height)
-		laser.y > canvas.height && (laser.y = 0)
-	})
+	draw_space()
 
-	asteroids_list.forEach((asteroid) => {
-		asteroid.x += asteroid.xv
-		asteroid.y += asteroid.yv
+	draw_lives()
+	draw_high_score()
+	draw_score()
 
-		asteroid.x < -asteroid.r && (asteroid.x = canvas.width + asteroid.r)
-		asteroid.x > canvas.width + asteroid.r && (asteroid.x = -asteroid.r)
-		asteroid.y < -asteroid.r && (asteroid.y = canvas.height + asteroid.r)
-		asteroid.y > canvas.height + asteroid.r && (asteroid.y = -asteroid.r)
-	})
-}, 1000 / fps)
+	draw_text()
+
+	move_ship()
+	draw_ship()
+
+	move_lasers()
+	draw_lasers()
+
+	move_asteroids()
+	draw_asteroids()
+	break_asteroids()
+}, 16)
